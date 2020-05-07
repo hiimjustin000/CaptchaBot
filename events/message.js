4614c14ae4112a1187cf251ad198c19df273b67e
hiimjustin000 <52604018+hiimjustin000@users.noreply.github.com>
2020-05-07 09:34:40 -0400

    No bots!

--- events/message.js
+++ events/message.js
@@ -1,12 +1,15 @@
 const db = require("quick.db");
 
 module.exports = async (client, message) => {
+    if (message.author.bot) return;
+    if (message.channel.type == "dm" || message.channel.type == "group") return;
+	
     let prefix = db.get(`prefix_${message.guild.id}`) != null ? db.get(`prefix_${message.guild.id}`) : process.env.PREFIX;
     let prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${prefix.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")})\\s*`);
-	if (!prefixRegex.test(message.content)) return;
+    if (!prefixRegex.test(message.content)) return;
 
-	let [, matchedPrefix] = message.content.match(prefixRegex);
-	let args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
+    let [, matchedPrefix] = message.content.match(prefixRegex);
+    let args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
     let command = args.shift().toLowerCase();
     
     let commandFile = client.commands[command];
