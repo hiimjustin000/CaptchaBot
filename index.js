5d4c3de11a46f6a647008372754bcca3880664b4
hiimjustin000 <justincpridgen672@gmail.com>
2020-06-08 14:09:16 -0400

    Someone told me this would get the bot to work 24/7

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const { Client } = require("discord.js");
+const client = new Client();
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const app = express();
+client.commands = {};
 
 app.use(express.static("public"));
 
@@ -9,20 +15,11 @@ app.get("/", (request, response) => {
   response.sendStatus(200);
 });
 
-let listener = http.createServer(app).listen(process.env.PORT, async () => {
-  console.log(`Bot is listening on port ${listener.address().port}`);
-});
+app.listen(process.env.PORT);
 
 setInterval(() => {
-    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me`);
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me`);
 }, 280000);
-
-const { Client } = require("discord.js");
-const client = new Client();
-const fs = require("fs");
-const path = require("path");
-require("dotenv").config();
-client.commands = {};
 
 let commandDir = fs.readdirSync(path.resolve(__dirname, "commands"));
 let eventDir = fs.readdirSync(path.resolve(__dirname, "events"));
